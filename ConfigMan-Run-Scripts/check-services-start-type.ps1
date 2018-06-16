32b55bd9f8ca4fa40505d6340f07de0be879ae1a
Schultz <n9112616@providence.org>
2018-06-15 23:16:34 -0700

    initial commit

---
+++ ConfigMan-Run-Scripts/check-services-start-type.ps1
@@ -0,0 +1,48 @@
+<#
+.SYNOPSIS
+Checks the startup type of a specified service
+
+.DESCRIPTION
+
+.PARAMETER servicename
+Name of teh service to check.  This is the service name and not the display name.  For example, Windows Event Log service would be eventlog
+
+.INPUTS
+
+.OUTPUTS
+
+.NOTES
+FileName: check-services-start-type.ps1
+Author: Michael Schultz
+Contact: mschultz@necro-monkey.com
+Created: 20180615
+Modified: 
+Version: 1.0.0
+
+.EXAMPLE
+check-services-start-type.ps1 -servicename eventlog
+
+#>
+
+#-----Parameters-----
+
+Param(
+[Parameter(Mandatory=$True)]
+[string]$servicename
+)
+
+#-----Initializations and Module Imports-----
+
+#-----Variables-----
+
+$srvstart = Get-Service -Name $servicename | Select-Object -ExpandProperty starttype
+
+#-----Functions-----
+
+#-----Logging-----
+
+#-----Execution-----
+
+if ($srvstart -ne $null )
+{$srvstart}
+else {"Service Not Found"}  
